1ae742bddb02c52e806dc5f58fd909b35c2b2299
Arthur <arthursantos0017@gmail.com>
2025-08-25 20:42:29 -0300

    aula_047_exercicio

---
+++ aula_047_ex.py
@@ -0,0 +1,20 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+
+count_nome = nome.replace(" ", "")
+
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nome[::-1]}')
+    
+    if ' ' in nome:
+        print('Seu nome contém espaços.')
+    else:
+        print('Seu nome não contém espaços.')
+
+    print(f'Seu nome tem {len(count_nome)} letras.')
+    print(f'A primeira letra do seu nome é {nome[0]}.')
+    print(f'A última letra do seu nome é {nome[-1]}.')
+
+else:
+    print('Desculpe, você deixou campos vazios.')
